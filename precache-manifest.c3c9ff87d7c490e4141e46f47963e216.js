76f46d6e64927f5f564c217a68939b111a5e6046
Amit Nautiyal <amit.nautiyal@crssoftware.co.nz>
2019-07-10 14:33:04 +1200

    Updates

---
+++ precache-manifest.c3c9ff87d7c490e4141e46f47963e216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5605ad1c8f730fe466ac12478477f4f5",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "cbdab60602870b416e21",
+    "url": "/shopping-cart/static/css/main.fd10fa68.chunk.css"
+  },
+  {
+    "revision": "8243acc408e6a0cf0678",
+    "url": "/shopping-cart/static/js/2.2463cc64.chunk.js"
+  },
+  {
+    "revision": "cbdab60602870b416e21",
+    "url": "/shopping-cart/static/js/main.c565e242.chunk.js"
+  },
+  {
+    "revision": "3675f54918f9d621420d",
+    "url": "/shopping-cart/static/js/runtime~main.60a85d2f.js"
+  }
+]);
